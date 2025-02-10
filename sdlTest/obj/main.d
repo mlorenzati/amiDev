aba8ec640cab6a24eea5eab5e125d6d34633e323
Marcelo Lorenzati <mlorenzati@gmail.com>
2025-02-09 21:38:18 -0300

    added libSDL build, incomplete

---
+++ sdlTest/obj/main.d
@@ -0,0 +1,117 @@
+obj/main.o: C:/Lorenzati/dev/amiga/amiDev/sdlTest/main.c \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_config_amigaos.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/inttypes.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stdint.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/limits.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stddef.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_main.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_stdinc.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_config.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_platform.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_config_minimal.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stdarg.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sys/amigaos-va.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_log.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sys/types.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/time.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stdio.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stdlib.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sys/clib2_stdc.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/string.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/strings.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/ctype.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/begin_code.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/close_code.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_audio.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_error.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_endian.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_mutex.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_thread.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_rwops.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_cdrom.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_cpuinfo.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_events.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_active.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_keyboard.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_keysym.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_mouse.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_video.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_joystick.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_quit.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_loadso.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_timer.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_version.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_headers.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/setjmp.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/signal.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/locale.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/errno.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/dos.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_localebase.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/macros.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_utilitybase.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/math_fp_support.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_profile.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/debug.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/include/assert.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_protos.h \
+ C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_memory.h
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_config_amigaos.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/inttypes.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stdint.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/limits.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stddef.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_main.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_stdinc.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_config.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_platform.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_config_minimal.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stdarg.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sys/amigaos-va.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_log.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sys/types.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/time.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stdio.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/stdlib.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sys/clib2_stdc.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/string.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/strings.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/ctype.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/begin_code.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/close_code.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_audio.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_error.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_endian.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_mutex.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_thread.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_rwops.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_cdrom.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_cpuinfo.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_events.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_active.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_keyboard.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_keysym.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_mouse.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_video.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_joystick.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_quit.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_loadso.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_timer.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/sdl/SDL_version.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_headers.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/setjmp.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/signal.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/locale.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/errno.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/dos.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_localebase.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/macros.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_utilitybase.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/math_fp_support.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_profile.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/debug.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/include/assert.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_protos.h:
+C:/Lorenzati/dev/amiga/amiDev/libs/library/stdlib_memory.h:
